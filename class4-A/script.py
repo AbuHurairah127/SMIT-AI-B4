56054163c22b8d1cdbee14382762b2f8ab25e030
abuhurairah127 <dev.abuhurairah@gmail.com>
2026-04-01 16:51:12 +0500

    class4-A

---
+++ class4-A/script.py
@@ -0,0 +1,45 @@
+# Functions
+# Salami
+# Task - Function Body
+# Return Gift
+# def functionName():
+#     # code
+
+
+def sum():
+    a = 20
+    b = 30
+    print(a + b)
+
+
+sum()
+sum()
+sum()
+sum()
+sum()
+sum()
+sum()
+sum()
+sum()
+num1 = 20
+print("My","number","are","zoo","Elephant",num1) # Argument
+
+def doubleShah(num):
+    print(num*2) 
+
+doubleShah(10)
+doubleShah(20) 
+doubleShah(40)
+
+def subtraction(num1,num2):
+    print(num1-num2)
+
+subtraction(10,20)
+subtraction(20,10)
+subtraction(num1 = 50, num2 =100)
+subtraction(num2 =100, num1 = 50)
+
+print("Hello World",end="\t")
+print("Dummy Data")
+print("Questions")
+print("Testing")
