b9121f0fd8d754e5f90e491fbebfecc5fcc6c6a6
abuhurairah127 <dev.abuhurairah@gmail.com>
2026-04-09 17:03:16 +0500

    Class5-B

---
+++ class5-B/script.py
@@ -0,0 +1,53 @@
+students = ["Ali","Ahmad","Usman","Hassan"]
+
+print(students[0])
+# students[0] = "Ali Raza"
+print(students[0])
+print(students[1])
+print(students[2])
+print(students[3])
+
+print(len(students))
+
+# last index = no. of elem. in list - 1
+
+# loop
+# For
+# While
+# i = 0
+# while i < 5:
+#     print("Hello", i)
+#     i = i + 1
+
+
+
+# i = 1
+# while i <= 10:
+#     print(f"2 X {i} = {2 * i}")
+#     i = i + 1
+
+
+
+# i = 10
+# # while i > 0
+# while i >= 1:
+#     print(f"2 X {i} = {2 * i}")
+#     i = i - i
+
+
+
+i = 0
+k = 0
+while i <= 98:
+    print(i)
+    k = k + 1
+    i = i + 2
+
+print(k,"no. of even numbers printed")
+
+
+j = 0
+while j < 100:
+    if j % 2 == 0:
+        print("j=",j)
+    j = j + 1
